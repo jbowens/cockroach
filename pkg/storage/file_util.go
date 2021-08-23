4e0fb087664309ae63519f70eb544718fc958834
Jackson Owens <jackson@cockroachlabs.com>
2021-08-23 09:58:45 -0400

    safe file
    Release note (<category, see below>): <what> <show> <why>

--- pkg/storage/file_util.go
+++ pkg/storage/file_util.go
@@ -12,8 +12,13 @@ package storage
 
 import (
 	"bytes"
+	"fmt"
 	"io"
+	"strconv"
+	"strings"
 
+	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/errors/oserror"
 	"github.com/cockroachdb/pebble/vfs"
 )
 
@@ -49,4 +54,172 @@ func SafeWriteToFile(fs vfs.FS, dir string, filename string, b []byte) error {
 	}
 	defer fdir.Close()
 	return fdir.Sync()
+}
+
+// SafeFile provides an interface for safely reading and writing a
+// file that requires atomicity for file replacement. The vfs.FS
+// interface does not guarantee atomicity of renames when the
+// destination path already exists, making it difficult to atomically
+// update files when necessary.
+//
+// SafeFile implements a naming scheme using monotonically increasing
+// file numbers to indicate which file is the most recent.
+//
+// SafeFile is not safe for concurrent access. Its atomicity guarantees
+// are lost if multiple processes use the same safe file namespace
+// simultaneously.
+//
+// New clients should consider using the Engine to perform atomic writes
+// to keys, rather than introducing a new file with atomicity
+// requirements.
+type SafeFile struct {
+	fs              vfs.FS
+	dir             string
+	name            string
+	current         uint64
+	currentFilename string
+}
+
+// Open opens the file for reading, returning a vfs.File for the current
+// file.
+func (f *SafeFile) Open() (vfs.File, error) {
+	if f.current == 0 {
+		return nil, oserror.ErrNotExist
+	}
+	return f.fs.Open(f.fs.PathJoin(f.dir, f.currentFilename))
+}
+
+// Replace allows atomic replacement of the safe file. The provided
+// replaceFn function is called for populating the new contents of the
+// file. If the replaceFn function returns a non-nil error, replacement
+// is aborted and the written contents are forgotten. If the replaceFn
+// function returns a non-nil error, Replace will proceed with replacing
+// the file.
+//
+// If Replace returns a nil error, the replacement is guaranteed to be
+// durably persisted with the written contents and any future calls to
+// Open for the same safe file will return the new contents,
+func (f *SafeFile) Replace(replaceFn func(w io.Writer) error) error {
+	replacementFile, err := f.ReplaceAppend(replaceFn)
+	if err != nil {
+		return err
+	}
+	return replacementFile.Close()
+}
+
+// ReplaceAppend is identical to Replace but returns a vfs.File for the
+// replacement file, allowing the caller to continue to append to the
+// file after the atomic rename.
+func (f *SafeFile) ReplaceAppend(replaceFn func(w io.Writer) error) (vfs.File, error) {
+	tempPath := f.fs.PathJoin(f.dir, f.name+tempFileExtension)
+	tmp, err := f.fs.Create(tempPath)
+	if err != nil {
+		return nil, err
+	}
+	// Call the closure to write to the replacement file. If an error is
+	// returned, the replacement is aborted.
+	if err := replaceFn(tmp); err != nil {
+		return nil, errors.CombineErrors(err, tmp.Close())
+	}
+	// Sync the file before we perform any replacements.
+	if err := tmp.Sync(); err != nil {
+		return nil, errors.CombineErrors(err, tmp.Close())
+	}
+
+	// Rename the file to the next file number path. Since the
+	// destination path doesn't exist, there is no risk of a nonatomic
+	// rename.
+	next := f.current + 1
+	nextFilename := fmt.Sprintf("%s-%010d", f.name, next)
+	if err := f.fs.Rename(tempPath, f.fs.PathJoin(f.dir, nextFilename)); err != nil {
+		return nil, errors.CombineErrors(err, tmp.Close())
+	}
+	prev, prevFilename := f.current, f.currentFilename
+	f.current, f.currentFilename = next, nextFilename
+
+	// Remove the now obsolete previous file.
+	if prev > 0 {
+		if err := f.fs.Remove(f.fs.PathJoin(f.dir, prevFilename)); err != nil {
+			return nil, errors.CombineErrors(err, tmp.Close())
+		}
+	}
+
+	// Sync the directory to ensure the rename is persisted.
+	fdir, err := f.fs.OpenDir(f.dir)
+	if err != nil {
+		return nil, errors.CombineErrors(err, tmp.Close())
+	}
+	defer fdir.Close()
+	if err := fdir.Sync(); err != nil {
+		return nil, errors.CombineErrors(err, tmp.Close())
+	}
+	return tmp, nil
+}
+
+// OpenSafeFile opens a new SafeFile. If no file exists yet,
+// OpenSafeFile willl succeed but a subsequent call to its Open method
+// will return os.ErrNotExist.
+func OpenSafeFile(fs vfs.FS, dir, name string) (*SafeFile, error) {
+	ls, err := fs.List(dir)
+	if err != nil {
+		return nil, err
+	}
+	var highest uint64
+	var highestFilename string
+	for _, filename := range ls {
+		// If the file doesn't begin with the prefix name, it's not
+		// relevant.
+		if !strings.HasPrefix(filename, name) {
+			continue
+		}
+
+		// If the file is a temporary file leftover from an unfinished
+		// replacement, remove it.
+		if strings.HasSuffix(filename, tempFileExtension) {
+			if err := fs.Remove(fs.PathJoin(dir, filename)); err != nil {
+				return nil, err
+			}
+			continue
+		}
+
+		// Parse out the file number.
+		i := strings.LastIndexByte(filename, '-')
+		if i == -1 {
+			return nil, errors.Newf("safe file %q: unexpected file %q", name, filename)
+		}
+		v, err := strconv.ParseUint(filename, 10, 64)
+		if err != nil {
+			return nil, errors.CombineErrors(errors.Newf("safe file %q: unexpected file %q", name, filename), err)
+		}
+		// Zero is reserved.
+		if v == 0 {
+			return nil, errors.Newf("safe file %q: unexpected file %q", name, filename)
+		}
+
+		switch {
+		case v < highest:
+			// The file at filename is old, superceded by our current
+			// highest.
+			if err := fs.Remove(filename); err != nil {
+				return nil, err
+			}
+		case v == highest:
+			return nil, errors.Newf("safe file %q: unexpected duplicate number %d", name, v)
+		case v > highest:
+			// This file replaces the existing highest.
+			if highest != 0 {
+				if err := fs.Remove(highestFilename); err != nil {
+					return nil, err
+				}
+			}
+			highest, highestFilename = v, filename
+		}
+	}
+	return &SafeFile{
+		fs:              fs,
+		dir:             dir,
+		name:            name,
+		current:         highest,
+		currentFilename: highestFilename,
+	}, nil
 }
--- pkg/storage/file_util_test.go
+++ pkg/storage/file_util_test.go
@@ -11,6 +11,7 @@
 package storage
 
 import (
+	"errors"
 	"io"
 	"io/ioutil"
 	"os"
@@ -63,4 +64,111 @@ func TestSafeWriteToFile(t *testing.T) {
 	// Discard any unsynced writes.
 	mem.ResetToSyncedState()
 	require.Equal(t, []byte("Hello everyone"), readFile("foo/bar"))
+}
+
+func TestSafeFile(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	// Set up a 'foo/bar' safe file.
+	mem := vfs.NewMem()
+	require.NoError(t, mem.MkdirAll("foo", os.ModePerm))
+	sf, err := OpenSafeFile(mem, "foo", "bar")
+	require.NoError(t, err)
+
+	// Open should return ErrNotExist.
+	f, err := sf.Open()
+	require.Equal(t, os.ErrNotExist, err)
+	require.Nil(t, f)
+
+	// If the Replace's closure returns an error, that error should be
+	// returned.
+	replaceErr := errors.New("uh oh")
+	err = sf.Replace(func(w io.Writer) error {
+		if _, err := io.WriteString(w, "Hello world!"); err != nil {
+			return err
+		}
+		return replaceErr
+	})
+	require.Equal(t, replaceErr, err)
+
+	// And the file should still not exist.
+	f, err = sf.Open()
+	require.Equal(t, os.ErrNotExist, err)
+	require.Nil(t, f)
+
+	// Replacing the file without a replaceFn error should succeed.
+	require.NoError(t, sf.Replace(func(w io.Writer) error {
+		_, err := io.WriteString(w, "Hello world")
+		return err
+	}))
+
+	// And the first iteration should have file number 1.
+	ls, err := mem.List("foo")
+	require.NoError(t, err)
+	require.Equal(t, []string{"bar-0000000001"}, ls)
+
+	// Reading the file should read the correct contents.
+	f, err = sf.Open()
+	require.NoError(t, err)
+	b, err := ioutil.ReadAll(f)
+	require.NoError(t, err)
+	require.NoError(t, f.Close())
+	require.Equal(t, "Hello world", string(b))
+
+	// Replacing the contents again should succeed.
+	require.NoError(t, sf.Replace(func(w io.Writer) error {
+		_, err := io.WriteString(w, "Hi")
+		return err
+	}))
+
+	// The current file number should have incremented.
+	ls, err = mem.List("foo")
+	require.NoError(t, err)
+	require.Equal(t, []string{"bar-0000000002"}, ls)
+}
+
+func TestSafeFile_Syncs(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	// Use an in-memory FS that strictly enforces syncs.
+	mem := vfs.NewStrictMem()
+	syncDir := func(dir string) {
+		fdir, err := mem.OpenDir(dir)
+		require.NoError(t, err)
+		require.NoError(t, fdir.Sync())
+		require.NoError(t, fdir.Close())
+	}
+	readFile := func(sf *SafeFile) []byte {
+		f, err := sf.Open()
+		require.NoError(t, err)
+		b, err := ioutil.ReadAll(f)
+		require.NoError(t, err)
+		require.NoError(t, f.Close())
+		return b
+	}
+
+	require.NoError(t, mem.MkdirAll("foo", os.ModePerm))
+	syncDir("")
+	sf, err := OpenSafeFile(mem, "foo", "bar")
+	require.NoError(t, err)
+	require.NoError(t, sf.Replace(func(w io.Writer) error {
+		_, err := io.WriteString(w, "Hello world")
+		return err
+	}))
+
+	// Discard any unsynced writes to make sure we set up the test
+	// preconditions correctly.
+	mem.ResetToSyncedState()
+	require.Equal(t, []byte("Hello world"), readFile(sf))
+
+	// Use Replace to atomically, durably change the contents of the
+	// file.
+	require.NoError(t, sf.Replace(func(w io.Writer) error {
+		_, err := io.WriteString(w, "Hello everyone")
+		return err
+	}))
+
+	// Discard any unsynced writes.
+	mem.ResetToSyncedState()
+	require.Equal(t, []byte("Hello everyone"), readFile(sf))
 }
